10d0a015e3535ff0620a5d019e4c075301922a48
Sunpy <sunpy@osufx.com>
2017-12-07 12:32:34 +0100

    All functions into php

--- fringuellina.php
+++ fringuellina.php
@@ -1,3 +1,29 @@
 <?php
-echo 'Its been a while since ive done php. Let me hide my gun first.';
+//echo 'Its been a while since ive done php. Let me hide my gun first.';
+
+class Fringuellina {
+    public static function PrintPage(){
+
+    }
+
+    public static function PrintInfoPage(){
+
+    }
+
+    public static function PrintCakesSummary(){
+
+    }
+
+    public static function PrintEditCake(){
+
+    }
+
+    public static function RAPButton(){
+
+    }
+
+    public static function RAPCakesListButton(){
+
+    }
+}
 ?>
