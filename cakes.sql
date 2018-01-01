f35094a6dd5d11f120d202a8c679737785f07fd2
Sunpy <jonas_steinsvik@hotmail.no>
2018-01-01 06:27:37 +0100

    Birb <3

---
+++ cakes.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jan 01, 2018 at 06:26 AM
+-- Server version: 5.7.20-0ubuntu0.16.04.1
+-- PHP Version: 5.6.32-1+ubuntu16.04.1+deb.sury.org+2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ripple`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cakes`
+--
+
+CREATE TABLE `cakes` (
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `processes` json NOT NULL,
+  `detected` json NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cakes`
+--
+ALTER TABLE `cakes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cakes`
+--
+ALTER TABLE `cakes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
