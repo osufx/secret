10a3e65fa8b63ab5d1a9c3ee9d665a801c638a52
Sunpy <sunpy@osufx.com>
2018-01-08 13:25:25 +0100

    Open cake shop

--- fringuellina.php
+++ fringuellina.php
@@ -3,7 +3,89 @@
 
 class Fringuellina {
     public static function PrintPage(){
+        // Multiple pages
+		$pageInterval = 100;
+		$from = (isset($_GET["from"])) ? $_GET["from"] : 999;
+		$to = $from+$pageInterval;
+		$users = $GLOBALS['db']->fetchAll('SELECT * FROM users WHERE id >= ? AND id < ?', [$from, $to]);
+		$groups = $GLOBALS["db"]->fetchAll("SELECT * FROM privileges_groups");
 
+        // Print stuff
+		echo '<div id="wrapper">';
+		printAdminSidebar();
+		echo '<div id="page-content-wrapper">';
+		// Maintenance check
+		P::MaintenanceStuff();
+		// Print Success if set
+		if (isset($_GET['s']) && !empty($_GET['s'])) {
+			P::SuccessMessageStaccah($_GET['s']);
+		}
+		// Print Exception if set
+		if (isset($_GET['e']) && !empty($_GET['e'])) {
+			P::ExceptionMessageStaccah($_GET['e']);
+        }
+        
+		// Get values
+		//$wm = current($GLOBALS['db']->fetch("SELECT value_int FROM system_settings WHERE name = 'website_maintenance'"));
+		
+		echo '<p align="center"><font size=5><i class="fa fa-birthday-cake"></i>	Cakes</font></p><br>';
+		// Quick edit/silence/kick user button
+		echo '<div><p align="center"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#quickLookupUserModal">Quick lookup user (username)</button>';
+		echo '&nbsp;&nbsp; <button type="button" class="btn btn-info" data-toggle="modal" data-target="#quickLookupUseridModal">Quick lookup user (user_id)</button>';
+		echo '&nbsp;&nbsp; <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#quickLookupScoreidModal">Quick lookup user (score_id)</button>';
+		echo '</p>';
+		// Users plays table
+		echo '<table class="table table-striped table-hover table-50-center">
+		<thead>
+		<tr><th class="text-center"><i class="fa fa-user"></i>	ID</th><th class="text-center">Username</th><th class="text-center">Cakes</th><th class="text-center">Toppings found</th><th class="text-center">Allowed</th><th class="text-center">Actions</th></tr>
+		</thead>
+		<tbody>';
+		foreach ($users as $user) {
+
+			// Get group color/text
+			$groupColor = "default";
+			$groupText = "None";
+			foreach ($groups as $group) {
+				if ($user["privileges"] == $group["privileges"] || $user["privileges"] == ($group["privileges"] | Privileges::UserDonor)) {
+					$groupColor = $group["color"];
+					$groupText = $group["name"];
+				}
+			}
+
+			// Get allowed color/text
+			$allowedColor = "success";
+			$allowedText = "Ok";
+			if (($user["privileges"] & Privileges::UserPublic) == 0 && ($user["privileges"] & Privileges::UserNormal) == 0) {
+				// Not visible and not active, banned
+				$allowedColor = "danger";
+				$allowedText = "Banned";
+			} else if (($user["privileges"] & Privileges::UserPublic) == 0 && ($user["privileges"] & Privileges::UserNormal) > 0) {
+				// Not visible but active, restricted
+				$allowedColor = "warning";
+				$allowedText = "Restricted";
+			} else if (($user["privileges"] & Privileges::UserPublic) > 0 && ($user["privileges"] & Privileges::UserNormal) == 0) {
+				// Visible but not active, disabled (not supported yet)
+				$allowedColor = "default";
+				$allowedText = "Locked";
+            }
+            
+            $cakes = current($GLOBALS['db']->fetch('SELECT COUNT(*) FROM cakes WHERE userid = ?', [$user["id"]]));
+
+            $toppings = current($GLOBALS['db']->fetch('SELECT COUNT(*) FROM cakes WHERE userid = ? AND detected NOT LIKE ?', [$user["id"], '[]']));
+
+			// Print row
+			echo '<tr>';
+			echo '<td><p class="text-center">'.$user['id'].'</p></td>';
+			echo '<td><p class="text-center"><b>'.$user['username'].'</b></p></td>';
+			echo '<td><p class="text-center">'.$cakes.'</p></td>';
+			echo '<td><p class="text-center">'.$toppings.'</p></td>';
+            echo '<td><p class="text-center"><span class="label label-'.$allowedColor.'">'.$allowedText.'</span></p></td>';
+            echo '<td><p class="text-center"><span class="label label-'.$allowedColor.'">EDIT</span></p></td>';
+			echo '</tr>';
+		}
+		echo '</tbody></table>';
+		echo '<p align="center"><a href="index.php?p=128&from='.($from-($pageInterval+1)).'">< Previous page</a> | <a href="index.php?p=128&from='.($to).'">Next page ></a></p>';
+		echo '</div>';
     }
 
     public static function PrintInfoPage(){
